7b1377f8ce27cd69a3ddeec1fd1b1b92508bee09
Mouse <farrasaddar@gmail.com>
2025-08-03 15:00:27 +0600

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const { Client } = require('whatsapp-web.js');
+const fs = require('fs');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-  puppeteer: { headless: true, args: ['--no-sandbox','--disable-setuid-sandbox'] }
+    authStrategy: new LocalAuth(),
+    puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
 });
 
 client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
-  console.log('QR RECEIVED');
+    qrcode.generate(qr, { small: true });
+    console.log('QR RECEIVED');
 });
 
 client.on('ready', () => {
-  console.log('Client is ready!');
+    console.log('Client is ready!');
 });
 
 client.on('message', msg => {
-  if (msg.body.toLowerCase() === '=halo') {
-    msg.reply('Halo juga! Aku bot aktif 🚀');
-  }
+    if (msg.body.toLowerCase() === '=halo') {
+        msg.reply('Halo juga! Aku bot aktif 🚀');
+    }
 });
 
 client.initialize();
